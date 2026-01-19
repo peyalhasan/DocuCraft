61ce2224f40a81e9f2d7f9d19681d360d7c6747b
Peyal Hasan <peyalhasan143@gmail.com>
2026-01-19 15:40:44 +0600

    Project Docucraft

--- app/error.js
+++ app/error.js
@@ -12,7 +12,7 @@ export default function Error({ error, reset }) {
         <div>
             <h2>Something went wrong!</h2>
             <h2>{error?.message}</h2>
-            <button
+            <button className='btn'
                 onClick={
                     // Attempt to recover by trying to re-render the segment
                     () => reset()
